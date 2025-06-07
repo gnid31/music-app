00b01750a3cc256c4d6b842d10506ba030af19bc
gnid31 <trancongdinh03012004@gmail.com>
2025-06-07 23:55:18 +0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -16,8 +16,8 @@ app.use(cors());
 // để xử lý JSON body của request
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use("/", routes);
-// app.use("/api", routes);
+// app.use("/", routes);
+app.use("/api", routes);
 
 app.use(errorHandler);
 
